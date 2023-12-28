9863ad68933e52aab8f809af791a73f65f44e139
marinos <marin@hospitalidee.fr>
2023-12-28 19:24:50 +0100

    Add start screen

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
-import pygame, asyncio
+import pygame, asyncio, os
 from sys import exit 
 import math
+
+class GameState:
+    RULES_SCREEN = 0
+    CHARACTER_SELECTION = 1
+    PLAYING = 2
+    GAME_OVER = 3
+
+current_game_state = GameState.CHARACTER_SELECTION
+
+async def start_screen():
+    start_font = pygame.font.Font("kongtext.ttf", 20)
+    welcome_text = "Bienvenue Aventurier, Dans ce jeu tu dois parcourir les 5 mondes (feu, eau, desert, etc...) et capturer l'esprit qui s'y trouve ! choisis un pouvoir qui te permettra la capture facile d'au moins un et combat les autres habilement"
+    welcome_surface = start_font.render(welcome_text, True, (255, 255, 255))
+    welcome_rect = welcome_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4 + 60))
+
+    start_button_text_surface = start_font.render("Press 'S' to start", True, (255, 255, 255))
+    start_button_rect = start_button_text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    return True
+
+        screen.fill((33, 33, 33))
+        screen.blit(welcome_surface, welcome_rect)
+        screen.blit(start_button_text_surface, start_button_rect)
+        pygame.display.flip()
+        await asyncio.sleep(0)
+
+async def end_screen(player_won):
+    end_font = pygame.font.Font("kongtext.ttf", 40)
+    result_text = "You Win!" if player_won else "Game Over"
+    result_surface = end_font.render(result_text, True, (255, 255, 255))
+    result_rect = result_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+
+    restart_text_surface = end_font.render("Press 'R' to restart", True, (255, 255, 255))
+    restart_rect = restart_text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    return True
+
+        screen.fill((33, 33, 33))
+        screen.blit(result_surface, result_rect)
+        screen.blit(restart_text_surface, restart_rect)
+        pygame.display.flip()
+        await asyncio.sleep(0)
+
 class Character:
     def __init__(self, name, power, damage, sheet_position, map_position, speed):
         self.name = name
@@ -33,22 +90,20 @@ CHARACTERS = [
 ]
 
 ENEMIES = [
-    Character(name="Infernus", power="fire", damage=10, sheet_position=(468,288), map_position=(0,0),speed=3),
-    Character(name="Galeblade", power="wind", damage=15, sheet_position=(312, 288), map_position=(1600,450),speed=4),
+    Character(name="Infernus", power="fire", damage=10, sheet_position=(468,288), map_position=(0,0),speed=2),
+    Character(name="Galeblade", power="wind", damage=15, sheet_position=(312, 288), map_position=(1600,450),speed=3),
     Character(name="Fulgur", power="thunder", damage=20, sheet_position=(156, 0), map_position=(1600,650),speed=2),
-    Character(name="Terravus", power="earth", damage=25, sheet_position=(0, 0), map_position=(1500,750),speed=3),
+    Character(name="Terravus", power="earth", damage=25, sheet_position=(0, 0), map_position=(1500,750),speed=2),
     Character(name="Tsunewave", power="water", damage=30, sheet_position=(0, 288), map_position=(1800,300),speed=3),
 ]
 
-# Define damage multipliers
 DAMAGE_MATRIX = {
-    'fire': {'wind': 2, 'water': 0.5, 'thunder': 1, 'earth': 0.3},
-    'wind': {'thunder': 2, 'earth': 0.5, 'water': 1, 'fire': 0.3},
-    'thunder': {'earth': 2, 'water': 0.5, 'fire': 1, 'wind': 0.3},
-    'earth': {'water': 2, 'fire': 0.5, 'wind': 1, 'thunder': 0.3},
-    'water': {'fire': 2, 'wind': 0.5, 'thunder': 1, 'earth': 0.3},
+    ("fire", "water"): 25,
+    ("wind", "fire"): 25,
+    ("thunder", "wind"): 25,
+    ("earth", "thunder"): 25,
+    ("water", "earth"): 25,
 }
-
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -71,7 +126,7 @@ ENEMY_SPEED = 3
 SHOOT_COOLDOWN = 40
 
 # Projectile settings
-PROJECTILE_SCALE = 1.4
+PROJECTILE_SCALE = 2.4
 PROJECTILE_SPEED = 8
 
 PROJECTILE_LIFETIME = 1200
@@ -160,28 +215,16 @@ class Player(pygame.sprite.Sprite):
         self.enemies_killed = 0
 
     def take_damage(self, projectile_power):
+        default_damage = 2
 
-        if self.power == "fire" and projectile_power == "water":
-            damage_taken = self.health - self.health / 2
-        elif self.power == "wind" and projectile_power == "fire":
-            damage_taken =self.health - self.health / 2
-        elif self.power == "thunder" and projectile_power == "wind":
-            damage_taken =self.health - self.health / 2
-        elif self.power == "earth" and projectile_power == "thunder":
-            damage_taken =self.health - self.health / 2
-        elif self.power == "water" and projectile_power == "earth":
-            damage_taken =self.health - self.health / 2
-        else:
-            damage_taken = 2
-
-        self.health = self.health - damage_taken
+        damage_taken = DAMAGE_MATRIX.get((self.power, projectile_power), default_damage)
+        self.health -= damage_taken
 
         # print(f"Player: {self.power}, projectile_power: {projectile_power}")
         # print(f"Player took {damage_taken} damage. Remaining health: {self.health}\n")
 
         if self.health <= 0:
-            pass
-            # print("Player defeated!")
+            current_game_state = GameState.GAME_OVER
 
     def load_sprites(self, sheet_position):
         sprite_width, sprite_height = 52, 72
@@ -326,30 +369,17 @@ class Enemy(pygame.sprite.Sprite):
         pygame.draw.rect(screen, (0, 255, 0), (health_bar_x, health_bar_y, health_bar_width, 5))
 
     def take_damage(self, projectile_power):
+        default_damage = 20
 
-        if self.power == "fire" and projectile_power == "water":
-            damage_taken = self.health = self.health / 2
-        elif self.power == "wind" and projectile_power == "fire":
-            damage_taken =self.health = self.health / 2
-        elif self.power == "thunder" and projectile_power == "wind":
-            damage_taken =self.health = self.health / 2
-        elif self.power == "earth" and projectile_power == "thunder":
-            damage_taken =self.health = self.health / 2
-        elif self.power == "water" and projectile_power == "earth":
-            damage_taken =self.health = self.health / 2
-        else:
-            damage_taken = 10
-
+        damage_taken = DAMAGE_MATRIX.get((self.power, projectile_power), default_damage)
         self.health -= damage_taken
 
-        # print(f"Enemy power: {self.power}, projectile_power: {projectile_power}")
-        # print(f"Enemy took {damage_taken} damage. Remaining health: {self.health}")
+        # print(f"Player: {self.power}, projectile_power: {projectile_power}")
+        # print(f"Player took {damage_taken} damage. Remaining health: {self.health}\n")
 
         if self.health <= 0:
-            # print("Enemy defeated!")
             player.enemies_killed += 1
-
-
+            self.kill()
 
     def hunt_player(self, player):
         player_vector = pygame.math.Vector2(player.rect.center)
@@ -493,14 +523,11 @@ class Camera(pygame.sprite.Group):
                 sprite.draw_health_bar(screen, offset_x, offset_y)
 
 class Projectile(pygame.sprite.Sprite):
-    def __init__(self, x, y, player_direction, angle = 0, power = "fire", shooter = "null"):
+    def __init__(self, x, y, player_direction, angle=0, power="fire", shooter="null"):
         super().__init__()
-        self.image = pygame.image.load("bullet/1.png").convert_alpha()
-        self.image = pygame.transform.rotozoom(self.image, 0, PROJECTILE_SCALE)
+
         self.direction = player_direction
-        self.angle = angle  # New angle parameter
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.angle = angle
         self.x = x
         self.y = y
         self.speed = PROJECTILE_SPEED
@@ -509,24 +536,37 @@ class Projectile(pygame.sprite.Sprite):
         self.power = power
         self.shooter = shooter
 
+        self.load_image()
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+        self.image = pygame.transform.scale(self.image, (24, 24))
+
+    def load_image(self):
+        power_mapping = {
+            "fire": "fire.png",
+            "water": "water.png",
+            "wind": "wind.png",
+            "earth": "earth.png",
+            "thunder": "thunder.png",
+        }
+
+        power_image_path = os.path.join("bullet", power_mapping.get(self.power, "1.png"))
+        self.image = pygame.image.load(power_image_path).convert_alpha()
 
     def handle_collisions(self, player, enemies):
-        # Check collision with player
         if self.shooter != player and self.rect.colliderect(player.rect):
             player.take_damage(projectile_power=self.power)
             self.kill()
 
-        # Check collision with enemies
         enemy_hits = pygame.sprite.spritecollide(self, enemies, False)
         for enemy in enemy_hits:
-            if self.shooter != enemy:  # Exclude the shooter
+            if self.shooter != enemy:
                 enemy.take_damage(projectile_power=self.power)
                 self.kill()
 
-
-    
     def projectile_movement(self):
-        # Calculate the new position based on angle
         self.x += self.speed * math.cos(self.angle)
         self.y += self.speed * math.sin(self.angle)
 
@@ -536,10 +576,10 @@ class Projectile(pygame.sprite.Sprite):
         if pygame.time.get_ticks() - self.spawn_time > self.projectile_lifetime:
             self.kill()
 
-
     def update(self):
         self.projectile_movement()
         self.handle_collisions(player, enemies)
+
 
 all_sprites_group = pygame.sprite.Group()
 projectile_group = pygame.sprite.Group()
@@ -579,18 +619,7 @@ async def character_selection_menu():
         await asyncio.sleep(0)
 
 async def main():
-    global all_sprites_group, projectile_group, enemy_group, selected_character, player
-
-    selected_character = await character_selection_menu()
-
-    player = Player(
-        sheet_position=selected_character.sheet_position,
-        power=selected_character.power,
-        damage=selected_character.damage,
-        speed=selected_character.speed
-    )
-    ui = UI(player)
-    all_sprites_group.add(player)
+    global all_sprites_group, projectile_group, enemy_group, selected_character, player, current_game_state
 
     while True:
         pygame.event.pump()  # Process events
@@ -600,14 +629,48 @@ async def main():
                 pygame.quit()
                 exit()
 
-        camera.custom_draw(player)
+        if current_game_state == GameState.CHARACTER_SELECTION:
+            start_game = await start_screen()
+            if start_game:
+                selected_character = await character_selection_menu()
+                player = Player(
+                    sheet_position=selected_character.sheet_position,
+                    power=selected_character.power,
+                    damage=selected_character.damage,
+                    speed=selected_character.speed
+                )
+                all_sprites_group.add(player)
+                ui = UI(player)
+                current_game_state = GameState.PLAYING
 
-        all_sprites_group.update()
-        ui.update()
+        elif current_game_state == GameState.PLAYING:
+            camera.custom_draw(player)
+
+            all_sprites_group.update()
+            ui.update()
+
+            # Check for win or lose conditions
+            if len(enemy_group) == 0:
+                current_game_state = GameState.GAME_OVER
+                player_won = True
+            elif player.health <= 0:
+                current_game_state = GameState.GAME_OVER
+                player_won = False
+
+        elif current_game_state == GameState.GAME_OVER:
+            restart = await end_screen(player_won)
+            if restart:
+                # Reset game state for a new game
+                current_game_state = GameState.CHARACTER_SELECTION
+                player.kill()
+                all_sprites_group.empty()
+                projectile_group.empty()
+                enemy_group.empty()
+                ui = UI(player)
+
         # Update the screen
         pygame.display.update()
         clock.tick(FPS)
         await asyncio.sleep(0)
-
 asyncio.run(main())
 
